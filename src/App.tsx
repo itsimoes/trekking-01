9f764e84fbf53be7a8ca86c53ca85d32de3625e6
Italo Simões <2d.italo@gmail.com>
2023-12-26 09:22:01 -0300

    Highlight01 component criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import Highlight01 from "./components/Highlights/Highlight01";
 
 function App() {
   return (
     <>
       <Home />
+      <Highlight01 />
     </>
   );
 }
---
+++ src/components/Highlights/Highlight01.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.scss"
+
+import Highlight01Detail from "../../assets/highlight01_detail.png";
+
+const Highlight01 = () => {
+  return (
+    <div className="wrapper highlights_wrapper">
+        <img src={Highlight01Detail} alt="" className="highlight01_detail"/>
+      <div className="container highlights_container">
+        <h2>1000+ Rotas. 1 milhão de Hikers. Momentos Inesquecíveis.</h2>
+        <div className="highlight01_card">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nihil
+            quia, cumque repellat accusamus tempore obcaecati illum aperiam a
+            nesciunt sint ratione quod voluptas alias ab.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Highlight01;
