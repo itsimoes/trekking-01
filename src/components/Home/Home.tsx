1ebeb5451113d9b49d4ee54fb1e67a59e3c0a410
Italo Simões <2d.italo@gmail.com>
2024-01-08 09:07:28 -0300

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.scss";
-const Home = () => {
+const Home = () => { 
+
   return (
     <div id="home" className="wrapper home_wrapper">
       <div className="container home_container">
-        <div className="home_quote slide_in_left">
+        <div id="home_left_card" className="home_quote slide_in_left">
           <span>* * * * *</span>
           <p>
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
@@ -17,11 +18,8 @@ const Home = () => {
             <h2>MAIS HIKING</h2>
             <h3>Menos Stress</h3>
           </div>
-          <a className="home_button">
-            Saiba Mais
-        </a>
+          <a className="home_button">Saiba Mais</a>
         </div>
-        
       </div>
     </div>
   );
